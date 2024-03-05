bd703bc263a4b5ea3b9bcb11eb1401a918d0cd44
Seroygit <162133714+Seroygit@users.noreply.github.com>
2024-03-05 12:54:32 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Seroy Save the World, Afiq Handsome!'));
+app.get('/', (req, res) => res.send('Seroy Save the World, Afiq Very Handsome!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
